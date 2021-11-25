b5fefd875f8a91350e41cd23f6c41ed899c4505e
Evan2323 <justvan71@mail.ru>
2021-11-25 19:19:42 +0300

    11111

--- Kisik/Kisik.DataAccess/DBContexts/AlbinaContext.cs
+++ Kisik/Kisik.DataAccess/DBContexts/AlbinaContext.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Kisik.DataAccess.Core.Interfaces;
+using Kisik.DataAccess.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,16 @@ using System.Threading.Tasks;
 
 namespace Kisik.DataAccess.DBContexts
 {
-    public class AlbinaContext
+    public class AlbinaContext : DbContext, IContext
     {
+        public AlbinaContext(DbContextOptions<AlbinaContext> options) : base(options)
+        {
 
+        }
+
+        public DbSet<UserRto> Users { get; set; }
+        public DbSet<VirtualExpenseRto> VirtualExpenses { get; set; }
+        public DbSet<RejectExpenceRto> RejectExpences { get; set; }
+        public DbSet<AcceptExpenseRto> AcceptExpenses { get; set; }
     }
 }
