a6dc8794793398be0d5c85642aedcb7d4f867538
Am1go1 <ivantimofeev930@gmail.com>
2021-12-07 18:39:13 +0300

    dd
    dd

---
+++ Kisik/ClassLibrary3/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using Albina.BusinessLogic.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Albina.BusinessLogic.Core.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserInformationBlo> Register(UserIdentityBlo userIdentityBlo);
+        Task<UserInformationBlo> Auth(UserIdentityBlo userIdentityBlo);
+        Task<UserInformationBlo> Update(UserUpdateBlo userUpdateBlo, UserIdentityBlo userIdentityBlo);
+        Task<UserInformationBlo> Get(int userId);
+        Task<bool> DoesExist(int numberPrefix, int number);
+    }
+}
---
+++ Kisik/Share/Exceptions/NoteFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shareds.Exceptions
+{
+    public class NoteFoundException : Exception
+    {
+        public NoteFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
